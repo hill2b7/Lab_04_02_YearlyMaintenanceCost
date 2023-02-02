f74123b5a44afe8b955d878e30c6eb9cb5eafd02
hill2b7 <hill2b7@mail.uc.edu>
2023-02-02 09:02:18 -0500

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args)
     {
-        double winterCost = 108.94;
-        double springCost = 203.95;
-        double summerCost = 180.68;
-        double fallCost = 138.36;
-        double yearlyCost = 0;
+        double winterCost = 108.94; //assigns value to variable
+        double springCost = 203.95; //assigns value to variable
+        double summerCost = 180.68; //assigns value to variable
+        double fallCost = 138.36; //assigns value to variable
+        double yearlyCost = 0; //assigns value to variable
 
-        System.out.println("The cost for Winter is " + winterCost);
-        System.out.println("The cost for Spring is " + springCost);
-        System.out.println("The cost for Summer is " + summerCost);
-        System.out.println("The cost for Fall is " + fallCost);
-        yearlyCost = winterCost + springCost + summerCost + fallCost;
-        System.out.println("The total yearly cost is " +yearlyCost);
+        System.out.println("The cost for Winter is " + winterCost); //output for Winter Cost
+        System.out.println("The cost for Spring is " + springCost);//output for Spring Cost
+        System.out.println("The cost for Summer is " + summerCost); //output for Summer Cost
+        System.out.println("The cost for Fall is " + fallCost); //output for fall cost
+        yearlyCost = winterCost + springCost + summerCost + fallCost; // calculates total yearly cost
+        System.out.println("The total yearly cost is " +yearlyCost); //Output for yearly cost
     }
 }
